742f7adc1a6c861319324068f581ad0928804c88
Luuk Kenselaar <luuk2014@hotmail.com>
2018-06-21 12:59:37 +0200

    Added temporary website

---
+++ public/temp-website/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en" class="full-height">
+
+<head>
+    <title>Coming soon - Steam user information</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <style>
+        body, html {
+            height: 100%;
+            margin: 0;
+        }
+
+        .bgimg {
+            background: url("bg.jpeg");
+            height: 100%;
+            background-position: center;
+            background-size: cover;
+            position: relative;
+            color: white;
+            font-family: "Courier New", Courier, monospace;
+            font-size: 25px;
+        }
+
+        .topleft {
+            position: absolute;
+            top: 0;
+            left: 16px;
+        }
+
+        .middle {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            text-align: center;
+        }
+
+        hr {
+            margin: auto;
+            width: 40%;
+        }
+    </style>
+
+</head>
+<body
+
+    <div class="bgimg">
+
+        <div class="topleft">
+            <p>Steamuser.me</p>
+        </div>
+        <div class="middle">
+            <h1>COMING SOON</h1>
+        </div>
+
+    </div>
+
+</body>
+</html>
