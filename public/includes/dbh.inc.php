8a6fb67e52091a8ddc08d58df2acfd9b31b01f89
Luuk Kenselaar <luuk2014@hotmail.com>
2018-06-23 00:36:36 +0200

    Added the DB connect file

---
+++ public/includes/dbh.inc.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Luuk Kenselaar <luuk.kenselaar@protonmail.com>
+ * Date: 23-6-2018
+ * Time: 00:24
+ */
+
+require 'credentials.inc.php';
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn) {
+    echo "Connected!";
+} else {
+    echo "Not connected!";
+}
