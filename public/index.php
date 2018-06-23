77c98e9738f27508cacd6bcbf0935a6f0849901c
Umit Canbolat <umit@ucanbolat.nl>
2018-06-23 15:37:10 +0200

    Auto stash before merge of "master" and "origin/master"

--- public/index.php
+++ public/index.php
@@ -4,17 +4,17 @@ require 'includes/functions.inc.php';
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="Oryx Network">
-    <meta name="keywords" content="Oryx Network, Oryx, Network">
-    <meta name="author" content="C0DE-BUST3RS">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/bulma.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <title>Home - Oryx Network</title>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="description" content="Oryx Network">
+	<meta name="keywords" content="Oryx Network, Oryx, Network">
+	<meta name="author" content="C0DE-BUST3RS">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/bulma.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<title>Home - Oryx Network</title>
 </head>
 
 <body>
@@ -43,30 +43,30 @@ require 'includes/functions.inc.php';
         <div class="container has-text-centered">
             <div class="columns is-vcentered">
 
-                <div class="column is-one-third">
-                    <figure class="image is-4by3">
-                        <img src="img/homepage-image.jpg" alt="" style="border-radius: 5%"/>
-                    </figure>
-                </div>
+				<div class="column is-one-third">
+					<figure class="image is-4by3">
+						<img src="img/homepage-image.jpg" alt="" style="border-radius: 5%"/>
+					</figure>
+				</div>
 
-                <div class="column is-one-third">
-                    <h1 class="title is-2">
-                        Oryx Network
-                    </h1>
-                    <h2 class="subtitle is-4">
-                        Best place to make new friends
-                    </h2>
-                    <br>
-                </div>
+				<div class="column is-one-third">
+					<h1 class="title is-2">
+						Oryx Network
+					</h1>
+					<h2 class="subtitle is-4">
+						Best place to make new friends
+					</h2>
+					<br>
+				</div>
 
-                <div class="column is-one-third">
-                    <h4 class="title is-4">
-                        Don't have an account?
-                    </h4>
-                    <h5 class="subtitle is-5">
-                        Register Now!
-                    </h5>
-                    <form action="" method="POST">
+				<div class="column is-one-third">
+					<h4 class="title is-4">
+						Don't have an account?
+					</h4>
+					<h5 class="subtitle is-5">
+						Register Now!
+					</h5>
+					<form action="" method="POST">
 
                         <div class="field">
                             <div class="control has-icons-left">
@@ -75,52 +75,52 @@ require 'includes/functions.inc.php';
                                 <span class="icon is-small is-left">
 									<i class="fa fa-address-card"></i>
     							</span>
-                            </div>
-                        </div>
+								</div>
+							</div>
 
-                        <div class="field">
-                            <div class="control has-icons-left">
-                                <input class="input is-primary is-normal is-rounded" id="registerLastname"
-                                       name="lastname" type="text" placeholder="Lastname">
-                                <span class="icon is-small is-left">
+							<div class="field">
+								<div class="control has-icons-left">
+									<input class="input is-primary is-normal is-rounded" id="registerLastname"
+										   name="lastname" type="text" placeholder="Lastname">
+									<span class="icon is-small is-left">
 									<i class="fa fa-address-card"></i>
     							</span>
-                            </div>
-                        </div>
+								</div>
+							</div>
 
-                        <div class="field">
-                            <div class="control has-icons-left">
-                                <input class="input is-primary is-normal is-rounded" id="registerEmail"
-                                       name="email" type="text" placeholder="Email">
-                                <span class="icon is-small is-left">
+							<div class="field">
+								<div class="control has-icons-left">
+									<input class="input is-primary is-normal is-rounded" id="registerEmail"
+										   name="email" type="text" placeholder="Email">
+									<span class="icon is-small is-left">
 									<i class="fa fa-envelope"></i>
     							</span>
-                            </div>
-                        </div>
+								</div>
+							</div>
 
-                        <div class="field">
-                            <div class="control has-icons-left">
-                                <input class="input is-primary is-normal is-rounded" id="registerMobile"
-                                       name="mobile" type="text" placeholder="Mobile">
-                                <span class="icon is-small is-left">
+							<div class="field">
+								<div class="control has-icons-left">
+									<input class="input is-primary is-normal is-rounded" id="registerMobile"
+										   name="mobile" type="text" placeholder="Mobile">
+									<span class="icon is-small is-left">
 									<i class="fa fa-phone"></i>
     							</span>
-                            </div>
-                        </div>
+								</div>
+							</div>
 
-                        <div class="field">
-                            <div class="control has-icons-left">
-                                <input class="input is-primary is-normal is-rounded" id="registerPassword"
-                                       name="password" type="password" placeholder="Password">
-                                <span class="icon is-small is-left">
+							<div class="field">
+								<div class="control has-icons-left">
+									<input class="input is-primary is-normal is-rounded" id="registerPassword"
+										   name="password" type="password" placeholder="Password">
+									<span class="icon is-small is-left">
 									<i class="fa fa-lock"></i>
     							</span>
                             </div>
                         </div>
 
-                        <a class="button is-danger is-outlined is-rounded">Register</a>
-                    </form>
-                </div>
+							<a class="button is-danger is-outlined is-rounded">Register</a>
+					</form>
+				</div>
 
             </div>
         </div>
