536d70dee57939403a65d861f95ea0124bd0c7d1
Luuk Kenselaar <luuk2014@hotmail.com>
2018-06-23 15:07:21 +0200

    Added some extra fields in the form

--- public/index.php
+++ public/index.php
@@ -1,128 +1,136 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport"
-		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<meta name="description" content="Oryx Network">
-	<meta name="keywords" content="Oryx Network, Oryx, Network">
-	<meta name="author" content="C0DE-BUST3RS">
-	<link rel="stylesheet" href="css/main.css">
-	<link rel="stylesheet" href="css/bulma.css">
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<title>Home - Oryx Network</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Oryx Network">
+    <meta name="keywords" content="Oryx Network, Oryx, Network">
+    <meta name="author" content="C0DE-BUST3RS">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/bulma.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <title>Home - Oryx Network</title>
 </head>
 
 <body>
 <section class="hero is-fullheight is-default is-bold">
-	<div class="hero-head">
-		<nav class="navbar is-fixed-top">
-			<div class="container">
-				<div class="navbar-brand">
-					<a class="navbar-item" href="../">
-						<img src="https://bulma.io/images/bulma-logo.png" alt="Logo">
-					</a>
-					<span class="navbar-burger burger" data-target="navbarMenu">
-            			<span></span>
-                        <span></span>
-                        <span></span>
-					</span>
-				</div>
-				<div id="navbarMenu" class="navbar-menu">
-					<div class="navbar-end">
-						<div class="tabs is-right">
-							<ul>
-								<li class="">
-									<form>
-										<div class="field is-grouped">
-											<p class="control has-icons-left">
-												<input class="input is-rounded" type="text" placeholder="Username">
-												<span class="icon is-small is-left">
-												  <i class="fa fa-user"></i>
-												</span>
-											</p>
-											<p class="control has-icons-left">
-												<input class="input is-rounded" type="password" placeholder="Password">
-												<span class="icon is-small is-left">
-												  <i class="fa fa-key"></i>
-												</span>
-											</p>
-										</div>
-									</form>
-								</li>
-								<li class="">
-									<a class="button is-success is-outlined is-rounded">Login</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</nav>
-	</div>
-	<div class="hero-body">
-		<div class="container has-text-centered">
-			<div class="columns is-vcentered">
+    <div class="hero-head">
+        <nav class="navbar is-fixed-top">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="#">
+                        <img src="https://bulma.io/images/bulma-logo.png" alt="Logo">
+                    </a>
+                    <a class="navbar-item" href="#">
+                        Home
+                    </a>
+                    <a class="navbar-item" href="#">
+                        Privacy
+                    </a>
+                    <a class="navbar-item" href="#">
+                        Contact
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="hero-body">
+        <div class="container has-text-centered">
+            <div class="columns is-vcentered">
 
-				<div class="column is-one-third">
-					<figure class="image is-4by3">
-						<img src="https://picsum.photos/800/600/?image=1045" alt=""/>
-					</figure>
-				</div>
+                <div class="column is-one-third">
+                    <figure class="image is-4by3">
+                        <img src="img/homepage-image.jpg" alt="" style="border-radius: 5%"/>
+                    </figure>
+                </div>
 
-				<div class="column is-one-third">
-					<h1 class="title is-2">
-						Oryx Network
-					</h1>
-					<h2 class="subtitle is-4">
-						Best place to make new friends
-					</h2>
-					<br>
-				</div>
+                <div class="column is-one-third">
+                    <h1 class="title is-2">
+                        Oryx Network
+                    </h1>
+                    <h2 class="subtitle is-4">
+                        Best place to make new friends
+                    </h2>
+                    <br>
+                </div>
 
-				<div class="column is-one-third">
-					<h4 class="title is-4">
-						Don't have an account?
-					</h4>
-					<h5 class="subtitle is-5">
-						Register Now!
-					</h5>
-					<form action="" method="POST">
+                <div class="column is-one-third">
+                    <h4 class="title is-4">
+                        Don't have an account?
+                    </h4>
+                    <h5 class="subtitle is-5">
+                        Register Now!
+                    </h5>
+                    <form action="" method="POST">
 
-						<div class="field">
-							<label class="label">Email</label>
-							<div class="control has-icons-left has-icons-right">
-								<input class="input" id="registerEmail" name="registerEmail" type="text" placeholder="Email">
-								<span class="icon is-small is-left">
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input class="input is-primary is-normal is-rounded" id="registerFirstname"
+                                       name="firstname" type="text" placeholder="Firstname">
+                                <span class="icon is-small is-left">
+									<i class="fa fa-address-card"></i>
+    							</span>
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input class="input is-primary is-normal is-rounded" id="registerLastname"
+                                       name="lastname" type="text" placeholder="Lastname">
+                                <span class="icon is-small is-left">
+									<i class="fa fa-address-card"></i>
+    							</span>
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input class="input is-primary is-normal is-rounded" id="registerEmail"
+                                       name="email" type="text" placeholder="Email">
+                                <span class="icon is-small is-left">
 									<i class="fa fa-envelope"></i>
     							</span>
+                            </div>
+                        </div>
 
-								<span class="icon is-small is-left">
-      								<i class="fa fa-email"></i>
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input class="input is-primary is-normal is-rounded" id="registerMobile"
+                                       name="mobile" type="text" placeholder="Mobile">
+                                <span class="icon is-small is-left">
+									<i class="fa fa-phone"></i>
     							</span>
-							</div>
+                            </div>
+                        </div>
 
-							<div class="field">
-								<a class="button is-danger is-outlined is-rounded">Register</a>
-							</div>
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input class="input is-primary is-normal is-rounded" id="registerPassword"
+                                       name="password" type="password" placeholder="Password">
+                                <span class="icon is-small is-left">
+									<i class="fa fa-lock"></i>
+    							</span>
+                            </div>
+                        </div>
 
-						</div>
-					</form>
-				</div>
+                        <a class="button is-danger is-outlined is-rounded">Register</a>
+                    </form>
+                </div>
 
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
 </section>
 <div class="hero-foot">
-	<div class="container">
-		<div class="tabs is-centered">
-			<ul>
-				<li><a>Copyright &copy; OryxNetwork 2018</a></li>
-			</ul>
-		</div>
-	</div>
+    <div class="container">
+        <div class="tabs is-centered">
+            <ul>
+                <li><a>Copyright &copy; OryxNetwork 2018</a></li>
+            </ul>
+        </div>
+    </div>
 </div>
 <script src="js/main.js"></script>
 </body>
