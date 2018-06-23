96d178f8132eea724ac305a7d9e1749eba1b2a6e
Umit Canbolat <umit@ucanbolat.nl>
2018-06-23 13:16:23 +0200

    Added fontawsome css + modified index.php

--- public/index.php
+++ public/index.php
@@ -10,9 +10,93 @@
     <meta name="author" content="C0DE-BUST3RS">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/bulma.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
     <title>Home - Oryx Network</title>
 </head>
-<body>
 
+<body>
+<section class="hero is-fullheight is-default is-bold">
+	<div class="hero-head">
+		<nav class="navbar is-fixed-top">
+			<div class="container">
+				<div class="navbar-brand">
+					<a class="navbar-item" href="../">
+						<img src="https://bulma.io/images/bulma-logo.png" alt="Logo">
+					</a>
+					<span class="navbar-burger burger" data-target="navbarMenu">
+            			<span></span>
+                        <span></span>
+                        <span></span>
+                        </span>
+				</div>
+				<div id="navbarMenu" class="navbar-menu">
+					<div class="navbar-end">
+						<div class="tabs is-right">
+							<ul>
+								<li class="">
+									<form>
+										<div class="field is-grouped">
+											<p class="control has-icons-left">
+												<input class="input is-rounded" type="text" placeholder="Username">
+												<span class="icon is-small is-left">
+												  <i class="fa fa-user"></i>
+												</span>
+											</p>
+											<p class="control has-icons-left">
+												<input class="input is-rounded" type="password" placeholder="Password">
+												<span class="icon is-small is-left">
+												  <i class="fa fa-key"></i>
+												</span>
+											</p>
+										</div>
+									</form>
+								</li>
+								<li class="">
+									<a class="button is-danger is-outlined is-rounded">Register</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</div>
+	<div class="hero-body">
+		<div class="container has-text-centered">
+			<div class="columns is-vcentered">
+				<div class="column is-5">
+					<figure class="image is-4by3">
+						<img src="https://picsum.photos/800/600/?random" alt="Description">
+					</figure>
+				</div>
+				<div class="column is-6 is-offset-1">
+					<h1 class="title is-2">
+						Oryx Network
+					</h1>
+					<h2 class="subtitle is-4">
+						Best place to make new friends
+					</h2>
+					<br>
+					<p class="has-text-centered">
+						<a class="button is-medium is-danger is-outlined is-rounded">
+							Register
+						</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="hero-foot">
+		<div class="container">
+			<div class="tabs is-centered">
+				<ul>
+					<li><a>Copyright &copy; OryxNetwork 2018</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
+<script src="js/main.js"></script>
 </body>
+
 </html>
