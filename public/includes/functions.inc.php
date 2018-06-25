a0cb2f3370db1f23815c4c27dd434640e9203e2d
Luuk Kenselaar <luuk2014@hotmail.com>
2018-06-25 15:36:23 +0200

    Added function

--- public/includes/functions.inc.php
+++ public/includes/functions.inc.php
@@ -6,143 +6,154 @@ require 'dbh.inc.php';
 //This function can be used at the signup and login
 function CheckIfEmailUsed($email)
 {
-	global $conn;
-	$sql = "SELECT * FROM user WHERE email = '$email';";
-	$result = $conn->query($sql);
-	$resultCheck = $result->num_rows;
+    global $conn;
+    $sql = "SELECT * FROM user WHERE email = '$email';";
+    $result = $conn->query($sql);
+    $resultCheck = $result->num_rows;
 
-	//If the email has been used
-	if ($resultCheck > 0) {
-		return true;
+    //If the email has been used
+    if ($resultCheck > 0) {
+        return true;
 
-		//If the email does not exist
-	} else {
-		return false;
-	}
+        //If the email does not exist
+    } else {
+        return false;
+    }
 }
 
 //This function will check if the user input at the Signup is not empty
 function CheckIfEmptySignup($firstname, $lastname, $email, $password)
 {
-	if (empty($firstname) || empty($lastname) || empty($email) || empty($password)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (empty($firstname) || empty($lastname) || empty($email) || empty($password)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 //This function will check if the user input at the Login is not empty
 function CheckIfEmptyLogin($email, $password)
 {
-	if (empty($email) || empty($password)) {
-		return true;
-	} else {
-		return false;
-	}
+    if (empty($email) || empty($password)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //This function will check if the users name is real
 function CheckIfRealName($firstname, $lastname)
 {
-	if (preg_match('/^[A-Za-z \'-]+$/i', $firstname) || preg_match('/^[A-Za-z \'-]+$/i', $lastname)) {
-		return true;
-	} else {
-		return false;
-	}
+    if (preg_match('/^[A-Za-z \'-]+$/i', $firstname) || preg_match('/^[A-Za-z \'-]+$/i', $lastname)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //This function will check if the users email is real
 function CheckIfRealEmail($email)
 {
-	if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-		return true;
-	} else {
-		return false;
-	}
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //This function will check if the users password has the minimum length
 function CheckIfPasswordLongEnough($password)
 {
-	if (strlen($password) >= 4) {
-		return true;
-	} else {
-		return false;
-	}
+    if (strlen($password) >= 4) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // This function wil check if the user is an admin or not.
 function CheckIfAdmin($userRank)
 {
-	if (isset($userRank) && !empty($userRank)) {
-		if ($userRank == 1) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    if (isset($userRank) && !empty($userRank)) {
+        if ($userRank == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 //This function will generate a random userid, example: F98F13DE-BABA5AFD-5AFDB4F9
 function GenerateUID()
 {
-	$s = strtoupper(md5(uniqid(rand(), true)));
-	$id =
-		substr($s, 0, 8) . '-' .
-		substr($s, 8, 8) . '-' .
-		substr($s, 12, 8);
-	return $id;
+    $s = strtoupper(md5(uniqid(rand(), true)));
+    $id =
+        substr($s, 0, 8) . '-' .
+        substr($s, 8, 8) . '-' .
+        substr($s, 12, 8);
+    return $id;
 }
 
 //This function will hash the user his password
 function HashPassword($nothashedPWD)
 {
-	$hashedPWD = password_hash($nothashedPWD, PASSWORD_DEFAULT);
-	return $hashedPWD;
+    $hashedPWD = password_hash($nothashedPWD, PASSWORD_DEFAULT);
+    return $hashedPWD;
 }
 
 // This function will un-hash the user password.
 function UnHashPassword($UserTypedPassword, $hashedPassword)
 {
-	if (password_verify($UserTypedPassword, $hashedPassword)) {
-		return true;
-	} else {
-		return false;
-	}
+    if (password_verify($UserTypedPassword, $hashedPassword)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // This function wil check if the user is already logged in, ifso then redirect to feed page automaticly.
 function CheckIfLoggedIn()
 {
-	if (isset($_SESSION['user']['id']) && !empty($_SESSION['user']['id'])) {
-		//header("Location: ../feed.php");
-		return true;
-	} else {
-		return false;
-	}
+    if (isset($_SESSION['user']['id']) && !empty($_SESSION['user']['id'])) {
+        //header("Location: ../feed.php");
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //This function will generate the current date and put it inside a variable.
 function GetCurrentDate()
 {
-	date_default_timezone_set('Europe/Amsterdam');
-	$date = date('Y-m-d H:i:s');
-	return $date;
+    date_default_timezone_set('Europe/Amsterdam');
+    $date = date('Y-m-d H:i:s');
+    return $date;
 }
 
 //This function will logout the user
 function LogoutUser()
 {
-	session_start();
-	session_unset();
-	session_destroy();
+    session_start();
+    session_unset();
+    session_destroy();
 }
 
+//This function will echo the copyright year in the footer
 function CopyrightYear()
 {
-	if (date('Y') == 2018) {
-		echo "2018";
-	} elseif (date('Y') != 2018) {
-		echo "2018 - " . date('Y');
-	}
+    if (date('Y') == 2018) {
+        echo "2018";
+    } elseif (date('Y') != 2018) {
+        echo "2018 - " . date('Y');
+    }
+}
+
+//This function will generate a token that will be send to the user
+function GenerateToken()
+{
+    $string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    $string = str_shuffle($string);
+    $token = substr($string, 40);
+    $tokenfinal = str_shuffle($token);
+    return $tokenfinal;
 }
