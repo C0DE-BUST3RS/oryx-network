22614bc05cceb85deca930c14de22f18913984c9
Umit Canbolat <umit@ucanbolat.nl>
2018-06-25 12:35:42 +0200

    Added a Logout function and function to check if the user is an admin or not.

--- public/includes/logout.inc.php
+++ public/includes/logout.inc.php
@@ -1,18 +1,14 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: Luuk Kenselaar <luuk.kenselaar@protonmail.com>
- * Date: 23-6-2018
- * Time: 00:48
- */
-
+// Require all functions.
 require 'functions.inc.php';
 
-
+// Check if the user is logged in, ifso proceed further.
 If (CheckIfLoggedIn() == true) {
-    session_unset();
-    session_destroy();
+    // Calling a function to destory all sessions.
+	LogoutUser();
+	// Redirect user to the homepage.
     header("Location: ../index.php");
 } else {
+	// If the user was not logged in redirect also to homepage.
     header("Location: ../index.php");
 }
