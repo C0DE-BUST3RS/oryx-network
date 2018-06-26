9f213428a60f902a8afe919fccd9999e536a3d37
Luuk Kenselaar <luuk2014@hotmail.com>
2018-06-26 13:15:02 +0200

    Placed the navbars in a file

---
+++ public/includes/nav-nologin.php
@@ -0,0 +1,23 @@
+<div class="hero-head">
+    <nav class="navbar is-fixed-top" style="background: #ffffff">
+        <div class="container">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="#">
+                    <img src="img/logos/oryx-trans.png" alt="Logo">
+                </a>
+                <a class="navbar-item" href="index.php">
+                    <i class="fa fa-home" aria-hidden="true"></i> &nbsp;Home
+                </a>
+                <a class="navbar-item" href="privacy.php">
+                    <i class="fa fa-user-o" aria-hidden="true"></i> &nbsp;Privacy
+                </a>
+                <a class="navbar-item" href="api.php">
+                    <i class="fa fa-cogs" aria-hidden="true"></i> &nbsp;API
+                </a>
+                <a class="navbar-item" href="contact.php">
+                    <i class="fa fa-envelope-o" aria-hidden="true"></i> &nbsp;Contact
+                </a>
+            </div>
+        </div>
+    </nav>
+</div>
