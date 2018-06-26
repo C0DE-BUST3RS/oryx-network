be34624d000349fd0f8b7e15821d0785cb7f1301
Umit Canbolat <umit@ucanbolat.nl>
2018-06-26 23:35:04 +0200

    Fixed responsive menu when logged in.

--- public/includes/nav-login.php
+++ public/includes/nav-login.php
@@ -5,19 +5,21 @@
                 <a class="navbar-item" href="feed.php">
                     <img src="img/logos/oryx-trans.png" alt="Logo">
                 </a>
-                <a class="navbar-item" href="feed.php">
-                    <i class="fa fa-home" aria-hidden="true"></i> &nbsp;My Feed
-                </a>
-                <a class="navbar-item" href="privacy.php">
-                    <i class="fa fa-user-o" aria-hidden="true"></i> &nbsp;Privacy
-                </a>
-                <a class="navbar-item" href="api.php">
-                    <i class="fa fa-cogs" aria-hidden="true"></i> &nbsp;API
-                </a>
-                <a class="navbar-item" href="contact.php">
-                    <i class="fa fa-envelope-o" aria-hidden="true"></i> &nbsp;Contact
-                </a>
+				<span class="navbar-burger burger" data-target="navMenu">
+					<span></span>
+					<span></span>
+					<span></span>
+				  </span>
             </div>
+			<div id="navMenu" class="navbar-menu">
+				<div class="navbar-end">
+					<a href="feed.php" class="navbar-item <?php if (basename($_SERVER['PHP_SELF']) == 'feed.php') { echo 'is-active'; } ?>"><i class="fa fa-home" aria-hidden="true"></i>
+						&nbsp;Feed</a>
+					<a href="privacy.php" class="navbar-item <?php if (basename($_SERVER['PHP_SELF']) == 'privacy.php') { echo 'is-active'; } ?>"><i class="fa fa-user-o" aria-hidden="true"></i> &nbsp;Privacy</a>
+					<a href="api.php" class="navbar-item <?php if (basename($_SERVER['PHP_SELF']) == 'api.php') { echo 'is-active'; } ?>"><i class="fa fa-cogs" aria-hidden="true"></i> &nbsp;API</a>
+					<a href="contact.php" class="navbar-item <?php if (basename($_SERVER['PHP_SELF']) == 'contact.php') { echo 'is-active'; } ?>"><i class="fa fa-envelope-o" aria-hidden="true"></i> &nbsp;Contact</a>
+				</div>
+			</div>
         </div>
     </nav>
 </div>
