f48a4cf75cf1c9ece8909574bdbf16e0a480c680
Umit Canbolat <umit@ucanbolat.nl>
2018-07-03 21:44:47 +0200

    Added TinyMCE WYSIWYG Editor.

--- public/feed.php
+++ public/feed.php
@@ -6,7 +6,7 @@ If (CheckIfLoggedIn() == false) {
     header("Location: ../index.php");
 }
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -19,33 +19,64 @@ If (CheckIfLoggedIn() == false) {
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/bulma.css">
     <link rel="stylesheet" href="css/font-awesome.min.css">
-    <title><?php echo ucwords($_SESSION['user']['firstname']) . "'s Feed"; ?> - Oryx Network</title>
+
+	<title><?php echo ucwords($_SESSION['user']['firstname']) . "'s Feed"; ?> - Oryx Network</title>
+	<!-- TinyMCE WYSIWYG Editor -->
+	<script type="text/javascript" src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=qkl3ur6pbinq8wcnwumww78ep4232ddwba6vn0qqhfugd0g7"></script>
+	<script type="text/javascript">
+		tinymce.init({
+            selector: 'textarea',  // change this value according to your HTML
+            toolbar: 'undo redo styleselect bold italic alignleft aligncenter alignright bullist numlist outdent indent code',
+			max_height: 500,
+            max_width: 500,
+            min_height: 100,
+            min_width: 400,
+            statusbar: false
+        });
+	</script>
 </head>
 <body>
 <section class="hero is-fullheight is-default is-bold">
     <?php
-    require 'includes/nav-login.php';
+    	require 'includes/nav-login.php';
     ?>
     <div class="hero-body">
         <div class="container has-text-centered">
             <div class="columns is-vcentered">
 
-                <div class="column is-one-third">
+                <div class="column">
+
+					<article class="media is-fixed-top">
+						<figure class="media-left">
+							<p class="image is-64x64">
+								<img src="https://bulma.io/images/placeholders/128x128.png">
+							</p>
+						</figure>
+						<div class="media-content">
+							<div class="field">
+								<p class="control">
+									<textarea class="textarea" placeholder="Add a comment..."></textarea>
+								</p>
+							</div>
+							<nav class="level">
+								<div class="level-left">
+									<div class="level-item">
+										<a class="button is-info">Submit</a>
+									</div>
+								</div>
+								<div class="level-right">
+									<div class="level-item">
+										<label class="checkbox">
+											<input type="checkbox"> Press enter to submit
+										</label>
+									</div>
+								</div>
+							</nav>
+						</div>
+					</article>
 
                 </div>
 
-                <div class="column is-one-third">
-                    <?php
-                    echo "<h4 class='title is-4'>" . Greetings($_SESSION['user']['firstname']) . "</h4>";
-                    foreach ($_SESSION['user'] as $key => $item) {
-                        echo "<b>" . $key . ":</b><br> " . $item . "<br>";
-                    }
-                    ?>
-                </div>
-
-                <div class="column is-one-third">
-
-                </div>
             </div>
         </div>
     </div>
