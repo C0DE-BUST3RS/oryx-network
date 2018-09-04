c1fd751ff4e515ace632fa3b461ef370b2bb4f9e
Luuk Kenselaar <luuk2014@hotmail.com>
2018-09-04 11:05:24 +0200

    Added extra check
    Now you cant place a post if the textarea is empty

--- public/includes/post.inc.php
+++ public/includes/post.inc.php
@@ -4,27 +4,32 @@ require 'functions.inc.php';
 
 //Check if the user has been logged in
 If (!CheckIfLoggedIn()) {
-	header("Location: ../index.php");
+    header("Location: ../index.php");
+    exit();
 }
 
-if (isset($_POST['messageSubmit']) && isset($_SESSION['user']['id'])) {
-	global $conn;
+if (isset($_POST['messageSubmit']) && isset($_SESSION['user']['id']) && !empty($_POST['textarea'])) {
+    global $conn;
 
-	// Set timezone to Europe -> Amsterdam
-	date_default_timezone_set('Europe/Amsterdam');
+    // Set timezone to Europe -> Amsterdam
+    date_default_timezone_set('Europe/Amsterdam');
 
-	// Define POST variables to custom variables.
-	$postUserID = $conn->real_escape_string($_POST['userID']);
-	$postMessage = $conn->real_escape_string($_POST['textarea']);
+    // Define POST variables to custom variables.
+    $postUserID = $conn->real_escape_string($_POST['userID']);
+    $postMessage = $conn->real_escape_string($_POST['textarea']);
 
-	// Create datetime for SQL.
-	$date = date("Y-m-d H:i:s");
+    // Create datetime for SQL.
+    $date = date("Y-m-d H:i:s");
 
-	// Add a new post submitted by the user.
-	$stmt = $conn->prepare("INSERT INTO post (`id`, `date`, `user_id`, `likes`, `content`) VALUES (NULL, ?, ?, 0, ?)");
-	$stmt->bind_param("sss", $date, $postUserID, $postMessage);
-	$stmt->execute();
+    // Add a new post submitted by the user.
+    $stmt = $conn->prepare("INSERT INTO post (`id`, `date`, `user_id`, `likes`, `content`) VALUES (NULL, ?, ?, 0, ?)");
+    $stmt->bind_param("sss", $date, $postUserID, $postMessage);
+    $stmt->execute();
 
-	// Redirect user back to feed page.
-	header("Location: ../feed.php?action=post-successfull");
+    // Redirect user back to feed page.
+    header("Location: ../feed.php?post=placed");
+    exit();
+} else {
+    header("Location: ../feed.php?post=failed");
+    exit();
 }
