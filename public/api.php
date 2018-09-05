b1f575137e1c51722eed9c152c63601945b245af
Luuk Kenselaar <luuk2014@hotmail.com>
2018-09-05 23:12:23 +0200

    Added some basic html to the API page

--- public/api.php
+++ public/api.php
@@ -23,6 +23,60 @@ require 'includes/functions.inc.php';
     <?php
     CheckIfLoggedIn() ? require 'includes/nav-login.php' : require 'includes/nav-nologin.php';
     ?>
+    <div class="hero-body">
+        <div class="container has-text-centered">
+            <div class="columns is-vcentered">
+
+                <div class="column is-one-third">
+                    <h1 class="title is-1">Oryx Network</h1>
+                </div>
+
+                <div class="column is-one-third">
+                    <h1 class="title is-1">API</h1>
+                </div>
+
+                <div class="column is-one-third">
+                    <h1 class="title is-1">Request key</h1>
+                    <?php
+                    if (!CheckIfLoggedIn()) { ?>
+                        <div class='notification is-warning is-rounded'>
+                            You are not logged-in <br> Login to request a API key
+                        </div>
+                    <?php } else { ?>
+                        <form action="includes/keyrequest.inc.php" method="post">
+
+                            <div class="field">
+                                <div class="control has-icons-left">
+                                    <input class="input is-primary is-info is-rounded" name="requestEmail" type="email"
+                                           placeholder="Email" value="<?php EmailFillIn(); ?>" required/>
+                                    <span class="icon is-small is-left">
+									    <i class="fas fa-address-card"></i>
+    							    </span>
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <div class="control">
+                                    <textarea class="textarea is-info" placeholder="Why you need access to our api?"
+                                              name="requestReason" rows="2" required></textarea>
+                                </div>
+                            </div>
+
+                            <button type="submit" id="submit" name="submit"
+                                    class="button is-info is-outlined is-rounded">
+                                <i class="fas fa-envelope"></i>&nbsp;Request key
+                            </button>
+
+                        </form>
+                    <?php } ?>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
     <?php
     require 'includes/footer.php';
     ?>
