d425dcc9574992bac08d539db9981c5895fa2bb0
Luuk Kenselaar <luuk2014@hotmail.com>
2018-09-06 22:42:19 +0200

    Added privacy page content

--- public/privacy.php
+++ public/privacy.php
@@ -5,27 +5,57 @@ require 'includes/functions.inc.php';
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport"
-		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<meta name="description" content="Oryx Network">
-	<meta name="keywords" content="Oryx Network, Oryx, Network">
-	<meta name="author" content="C0DE-BUST3RS">
-	<link rel="stylesheet" href="css/main.css">
-	<link rel="stylesheet" href="css/bulma.css">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Oryx Network">
+    <meta name="keywords" content="Oryx Network, Oryx, Network">
+    <meta name="author" content="C0DE-BUST3RS">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/bulma.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
-	<title>Privacy - Oryx Network</title>
+    <title>Privacy - Oryx Network</title>
 </head>
 
 <body>
 <section class="hero is-fullheight is-default is-bold">
-	<?php
+    <?php
     CheckIfLoggedIn() ? require 'includes/nav-login.php' : require 'includes/nav-nologin.php';
     ?>
-	<?php
-	require 'includes/footer.php';
-	?>
+    <div class="hero-body">
+        <div class="container has-text-centered">
+            <div class="columns is-vcentered">
+
+                <div class="column is-one-quarter">
+                    <h1 class="title is-1"><i class="fas fa-user-secret"></i> Privacy</h1>
+                </div>
+
+                <div class="column is-one-quarter">
+                </div>
+
+                <div class="column is-half">
+                    <div class="content">
+                        <h1 class="subtitle">Lorem Ipsum</h1>
+                        <p>Lorem ipsum<sup><a>[1]</a></sup> dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque. Sub<sub>script</sub> works as well!</p>
+                        <h2 class="subtitle">Lorem Ipsum</h2>
+                        <p>Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong> blandit. Quisque condimentum maximus mi, sit amet commodo arcu rutrum id. Proin pretium urna vel cursus venenatis. Suspendisse potenti. Etiam mattis sem rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et neque nisl.</p>
+                        <ul>
+                            <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
+                            <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
+                            <li>Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.</li>
+                            <li>Ut non enim metus.</li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <?php
+    require 'includes/footer.php';
+    ?>
 </section>
 
 <script src="js/main.js"></script>
