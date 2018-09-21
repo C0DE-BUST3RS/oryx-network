9412a106ff536cb27f60d7c350cc4887e0478133
Umit Canbolat <umit@ucanbolat.nl>
2018-09-21 09:29:33 +0200

    WIP

--- public/admin/users-overview.php
+++ public/admin/users-overview.php
@@ -60,7 +60,66 @@ If (!CheckIfAdmin($_SESSION['user']['email'])) {
 							</header>
 							<div class="card-table">
 								<div class="content">
-									<!-- CONTENT INSIDE HERE -->
+									<!-- Start all users -->
+									<div class="content">
+										<table class="table is-fullwidth is-striped">
+											<thead>
+											<tr>
+												<th><abbr title="ID">#</abbr></th>
+												<th><abbr title="Firstname">Firstname</abbr></th>
+												<th><abbr title="Lastname">Lastname</abbr></th>
+												<th><abbr title="Rank">Rank</abbr></th>
+												<th><abbr title="RegisterDate">Register Date</abbr></th>
+												<th><abbr title="LastLogin">Last Login</abbr></th>
+												<th><abbr title="RegisterIP">Register IP</abbr></th>
+												<th><abbr title="LastIP">Last IP</abbr></th>
+												<th><abbr title="Email">Email</abbr></th>
+												<th><abbr title="Edit">Edit</abbr></th>
+											</tr>
+											</thead>
+											<tbody>
+
+											<?php
+											$stmt = $conn->prepare("SELECT u.id ,u.email ,u.firstname ,u.lastname, u.admin ,po.id ,po.date ,po.user_id ,po.likes ,po.content ,pr.profile_picture ,pr.intro FROM user u INNER JOIN post po on u.id = po.user_id INNER JOIN profiles pr on po.user_id = pr.user_id ORDER BY po.id DESC LIMIT 5");
+											$stmt->execute();
+											$result = $stmt->get_result();
+
+											while ($row = $result->fetch_assoc()) { ?>
+												<tr>
+													<td width="5%">
+														<img class="is-centered" src="<?php echo $row['profile_picture']; ?>" alt="">
+													</td>
+													<td>
+														<p><?php echo ucfirst($row['firstname']);?></p>
+													</td>
+													<td>
+														<p><?php echo ucfirst($row['lastname']);?></p>
+													</td>
+													<td>
+														<?php
+														if($row['admin'] === 1) { ?>
+															<span class="tag is-danger">Admin</span>
+														<?php } else { ?>
+															<span class="tag is-success">User</span>
+														<?php } ?>
+													</td>
+													<td>
+														<?php
+															echo date('d M Y, H.i.s A', strtotime($row['date']));
+														?>
+													</td>
+													<td><a class="button is-small is-primary" href="#">Action</a></td>
+													<td><a class="button is-small is-primary" href="#">Action</a></td>
+													<td><a class="button is-small is-primary" href="#">Action</a></td>
+													<td><a class="button is-small is-primary" href="#">Action</a></td>
+													<td><a class="button is-small is-primary" href="#">Action</a></td>
+												</tr>
+											<?php } ?>
+
+											</tbody>
+										</table>
+									</div>
+									<!-- End all users -->
 								</div>
 							</div>
 
