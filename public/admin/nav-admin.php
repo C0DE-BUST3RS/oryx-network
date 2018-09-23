394a2607c54e7e46a43b45b0db5694c31a38067c
Luuk Kenselaar <luuk2014@hotmail.com>
2018-09-23 23:38:18 +0200

    Changed the nav

--- public/admin/nav-admin.php
+++ public/admin/nav-admin.php
@@ -4,15 +4,20 @@
 	</p>
 	<ul class="menu-list">
 		<li><a href="index.php" <?php if ($_SERVER['PHP_SELF'] == "/admin/index.php") { ?> class="is-active" <?php } ?>">Dashboard</a></li>
-		<li><a href="users-overview.php" <?php if ($_SERVER['PHP_SELF'] == "/admin/users-overview.php") { ?> class="is-active" <?php } ?>>All Users</a></li>
 	</ul>
-	<p class="menu-label">
+    <p class="menu-label">
+        Users
+    </p>
+    <ul class="menu-list">
+        <li><a href="users-overview.php" <?php if ($_SERVER['PHP_SELF'] == "/admin/users-overview.php") { ?> class="is-active" <?php } ?>>User overview</a></li>
+    </ul>
+    <p class="menu-label">
 		API
 	</p>
 	<ul class="menu-list">
-        <li><a href="api-calls.php" <?php if ($_SERVER['PHP_SELF'] == "/admin/api-calls.php") { ?> class="is-active" <?php } ?>>Last API Calls</a></li>
+        <li><a href="api-calls.php" <?php if ($_SERVER['PHP_SELF'] == "/admin/api-calls.php") { ?> class="is-active" <?php } ?>>API Calls</a></li>
+        <li><a href="api-keys.php" <?php if ($_SERVER['PHP_SELF'] == "/admin/api-keys.php") { ?> class="is-active" <?php } ?>>API Keys</a></li>
         <li><a href="api-key-requests.php" <?php if ($_SERVER['PHP_SELF'] == "/admin/api-key-requests.php") { ?> class="is-active" <?php } ?>>New Key Requests</a></li>
-        <li><a href="api-keys.php" <?php if ($_SERVER['PHP_SELF'] == "/admin/api-keys.php") { ?> class="is-active" <?php } ?>>All API Keys</a></li>
     </ul>
 	<p class="menu-label">
 		Support
